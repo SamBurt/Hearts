de57b1c12f8ea964631536036a99c0cfd543ebb5
SamBurt <sburt@colgate.edu>
2018-12-04 17:33:11 -0500

    Need to code actual game

--- Game.py
+++ Game.py
@@ -1,6 +1,38 @@
 from Card import Card
 from Deck import Deck
+from Player import Player
 
 class Game:
-    deck = Deck()
-    deck.print_deck()
+
+    def __init__(self):
+        self.players = self.create_players()
+        deck = Deck()
+
+    def create_players(self):
+        players = []
+        valid = False
+        num_players = 0;
+        while not valid:
+            num_players = int(raw_input("How many people are playing? (2-6) --> "))
+            if ((num_players >= 2) and (num_players <= 6)):
+                valid = True
+            else:
+                print("Number not valid")
+        for i in range(num_players):
+            name = raw_input("What is the name of player " + str(i+1) + "? --> ")
+            players.append(Player(name))
+        return players
+
+    def print_players(self):
+        for player in self.players:
+            print(player)
+
+    def playGame(self):
+        
+
+
+if __name__ == '__main__':
+    print("Starting")
+    game = Game()
+    game.print_players()
+    # game.playGame()
--- Player.py
+++ Player.py
@@ -1,2 +1,24 @@
+from Card import Card
+from Deck import Deck
+
 class Player:
-    
+
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.hand = []
+
+    def __repr__(self):
+        return self.name + " has " + str(self.score) + " points"
+
+    def computer_score(self, points):
+        self.score = self.score + points
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def remove_card(self, card):
+        if card in self.hand:
+            self.hand.remove(card)
+        else:
+            EnvironmentError()
